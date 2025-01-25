ecdd35aedf986c9f2dc443166fc4dfa686f865a0
Gruskas <gruskaspl@gmail.com>
2025-01-25 16:10:18 +0100

    Fixed issue with loading thankyou-view.fxml

--- src/main/java/org/gruskas/odtmanager/ReportController.java
+++ src/main/java/org/gruskas/odtmanager/ReportController.java
@@ -1,5 +1,6 @@
 package org.gruskas.odtmanager;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 
@@ -7,12 +8,12 @@ public class ReportController {
     public void submitButton(ActionEvent event) {
         Stage currentStage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
         currentStage.close();
-        ViewLoader.loadModalView(
+        Platform.runLater(() -> ViewLoader.loadModalView(
                 Application.stage,
                 "fxml/thankyou-view.fxml",
                 "SettingsStyle.css",
                 "/org/gruskas/odtmanager/logo.png",
                 "OdtFileManager - Thank you"
-        );
+        ));
     }
 }
