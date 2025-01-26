81a1b860726d841a16571c3f583347426693e9b0
Gruskas <gruskaspl@gmail.com>
2025-01-26 14:46:25 +0100

    Added backup date setting

--- src/main/java/org/gruskas/odtmanager/SettingsController.java
+++ src/main/java/org/gruskas/odtmanager/SettingsController.java
@@ -5,7 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -15,6 +18,18 @@ public class SettingsController {
 
     @FXML
     private TextField inputTextField;
+
+    @FXML
+    private Pane dateInput;
+
+    @FXML
+    private Spinner<Integer> monthsSpinner;
+
+    @FXML
+    private Spinner<Integer> weeksSpinner;
+
+    @FXML
+    private Spinner<Integer> daysSpinner;
 
     @FXML
     private CheckBox archiveFiles;
@@ -40,6 +55,13 @@ public class SettingsController {
     public void initialize() {
         inputTextField.setText(ConfigFileManager.folderPath);
         archiveFiles.setSelected(ConfigFileManager.archiveFiles);
+        dateInput.setVisible(archiveFiles.isSelected());
+        archiveFiles.selectedProperty().addListener((_, _, selected) -> dateInput.setVisible(selected));
+
+        monthsSpinner.setValueFactory(new IntegerSpinnerValueFactory(0, 12, 0));
+        weeksSpinner.setValueFactory(new IntegerSpinnerValueFactory(0, 52, 0));
+        daysSpinner.setValueFactory(new IntegerSpinnerValueFactory(0, 365, 0));
+
     }
 
     private void refreshMainView() {
